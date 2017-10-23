8d49dfe41c48d4fa1baa800d4aee50102918a03d
baitianyu <baitianyu2009@gmail.com>
2017-10-23 09:50:11 +0800

    为 MCTableBaseDescribeData.swift  添加一个便利化初始化方法

--- HTMultiCellTypeTableViewSwift/MCTableBaseDescribeData.swift
+++ HTMultiCellTypeTableViewSwift/MCTableBaseDescribeData.swift
@@ -12,16 +12,22 @@ open class MCTableBaseDescribeData: NSObject {
 
     public var cellClass: AnyClass = UITableViewCell.self
     public weak var cell: MCTableBaseCell?
-    public var customCellClosure: ((MCTableBaseCell, MCTableBaseDescribeData) -> ())?
-    public var selectCellClosure: ((MCTableBaseCell, MCTableBaseDescribeData) -> ())?
+    public var customCellClosure: ((MCTableBaseCell, MCTableBaseDescribeData) -> Void)?
+    public var selectCellClosure: ((MCTableBaseCell, MCTableBaseDescribeData) -> Void)?
     
     override public init() {
         super.init()
         customCellClosure = self.defaultCustomCellClosure()
     }
     
-    private func defaultCustomCellClosure() -> ((MCTableBaseCell, MCTableBaseDescribeData) -> ()) {
-        let customClosure = { (cell: MCTableBaseCell, descirbeData: MCTableBaseDescribeData) -> () in
+    public convenience init(_ cellClass: AnyClass = UITableViewCell.self) {
+        self.init()
+        customCellClosure = self.defaultCustomCellClosure()
+        self.cellClass = cellClass
+    }
+    
+    private func defaultCustomCellClosure() -> ((MCTableBaseCell, MCTableBaseDescribeData) -> Void) {
+        let customClosure = { (cell: MCTableBaseCell, descirbeData: MCTableBaseDescribeData) -> Void in
             self.cell = cell
             cell.describeData = self
         }
